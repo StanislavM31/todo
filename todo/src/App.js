17438034bf6da94b1ce8f700e8fdae8cf9ae16c6
Stanislav Minko <minkostas@gmail.com>
2023-12-22 20:35:24 +0300

    :todo_0

---
+++ todo/src/App.js
@@ -0,0 +1,17 @@
+/* import logo from './logo.svg'; */
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          header for App
+      </header>
+      <body>
+        todo
+      </body>
+    </div>
+  );
+}
+
+export default App;
