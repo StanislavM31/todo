afbffb273314e310b5094938f8a76c1327a91531
Stanislav Minko <minkostas@gmail.com>
2023-12-23 21:41:07 +0300

    :addTodo

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,14 +1,39 @@
 /* import logo from './logo.svg'; */
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [text, setText] = useState("");
+  const addTodo = () => {
+    if (text.trim().length) {
+      setTodos([
+        ...todos,
+        {
+          id: new Date().toISOString(),
+          text,
+          completed: false,
+        },
+      ]);
+    }
+  };
   return (
     <div className="App">
-      <header className="App-header">
-          header for App
-      </header>
+      <header className="App-header">header for App</header>
       <body>
-        todo
+        <label htmlFor="">
+          <input type="text" onChange={(e) => setText(e.target.value)}/>
+          <button onClick={addTodo}>+Add</button>
+        </label>
+        <ul>
+          {todos.map((todo) => (
+            <li key={todo.id}>
+              <input type="checkbox" />
+              <span>{todo.text}</span>
+              <span className="delete">&times;</span>
+            </li>
+          ))}
+        </ul>
       </body>
     </div>
   );
